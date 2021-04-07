66bd88dbec04020865ae7bc53ecbd10633e3fadf
sulaiman <mdsulaiman132k@gmail.com>
2021-04-07 21:08:18 +0530

    new Updates

---
+++ javascript/Fooddesign.js
@@ -0,0 +1,36 @@
+jsarray = []
+
+$.ajax({
+    url: 'http://localhost:3000/api/v1/restaurants/'+ sessionStorage.getItem("restaurants" ),
+    method: 'GET',
+    dataType: 'json',
+    data : {page: 1  , data: 1 }  ,
+    success: function(data , status){
+            jsarray = data 
+            console.log(jsarray)  
+            loadRes(jsarray) 
+    }
+});
+
+const loadRes =  (jsarray) => {
+    $(".foodhead").html(jsarray[0].hotelname)
+    $(".fooddes").html(jsarray[0].hotel_cate)
+    $(".foodloc").html(jsarray[0].hotel_address)
+    $(".foodrating").html("<i class=\"far fa-star\"></i>           " + jsarray[0].rating)
+    $(".foodimg").attr( "src" , jsarray[0].hotelimg)
+}
+
+$(".btn-icre-decre").click(function(){
+    let val = $(".btn-icre-decre").html()
+
+   
+    if(val == "add"){
+       $(".btn-add").css("visibility" , "visible" )
+       $(".btn-sub").css("visibility" , "visible" )
+       $(".btn-icre-decre").html(1)
+    }
+});
+
+$(".btn-add").click(function(){
+   
+});
--- javascript/main.js
+++ javascript/main.js
@@ -13,6 +13,13 @@ else if(n < 15)
 else if(n < 18)
   t = "S"
 
+const privacycom = () => {
+    return ("<div class=\"container-fluiod\" style=\"margin-top:2rem;background-color: #e9e9eb;display: inline-block;height: 18rem;width: 100%;padding:2rem;line-height: 1.6;\">\
+    We ( Bundl Technologies Private Limited, alias \"FoodService\" ) are fully committed to respecting your privacy and shall ensure that your personal information is safe with us. This privacy policy sets out the information practices of www.FoodService.com (\"Website\") including the type of information is collected, how the information is collected, how the information is used and with whom it is shared. Reference to \"you\" in this Privacy Policy refers to the users of this Website whether or not you access the services available on the Website or consummate a transaction on the Website. By using or accessing this Website, you agree to the terms and conditions of this Privacy Policy. You also expressly consent to our use and disclosure of your Personal Information (as defined below) in any manner as described in this Privacy Policy and further signify your agreement to this Privacy Policy and the Terms of Use (being incorporated by reference herein). If you do not agree with the terms and conditions of this Privacy Policy, please do not proceed further or use or access this Website.\
+  </div>")
+  }
+  
+
 console.log(t , n)
 const sendreqforresdata = (reqdata , reqtime) => {
   $.ajax({
@@ -31,12 +38,15 @@ const sendreqforresdata = (reqdata , reqtime) => {
                 We can’t find anything related va to your search.<br>\
                 Try a different search.</div></center>")
               }
+              $(".privacy").html(privacycom())
             }
     },
     error: function(status){
+     
       $("#restaurantContainer").html("<center><div style='position:relative;left:-40px'><h5>Oh! There’s not much left</h5><br>\
       We can’t find anything related to your search.<br>\
       Try a different search.</div></center>")
+      $(".privacy").html(privacycom())
     },
     async : false 
   });
@@ -56,6 +66,7 @@ const searchingquery = (reqsearch , reqdata , reqtime ) =>{
                 We can’t find anything related to your search.<br>\
                 Try a different search.</div></center>")
             }
+            else if(jsarray.length == 0)  $(".privacy").html(privacycom())
             else{
             render(jsarray) }
     },
@@ -71,7 +82,7 @@ const render = (data) => {
   scroll++
   searchscroll++ ;
   for(var i = 0 ; i < data.length ;i++){
-   component += "<div id= \"" + data[i].id +"\"    class =\"col col-lg-3 col-md-3 col-sm-4 bg-light\">"  + " <img class = \"imgitems\" src=" + 
+   component += "<div id= \"rest" + data[i].id +"\"    class =\"col col-lg-3 col-md-3 col-sm-4 bg-light\">"  + " <img class = \"imgitems\" src=" + 
       data[i].hotelimg + "/>" + "<h5 class =\"hot-name\">" +  data[i].hotelname + "</h5>" + "<p class=\"hot-cat\">" +
       data[i].hotel_cate + "</p>" + "<p><span class=\"hot-rating\" ><i class=\"far fa-star\"></i>" + 
       data[i].rating + "</span></p> <hr /></a> </div>"
@@ -82,6 +93,7 @@ const render = (data) => {
   }
   else{
     $("#restaurantContainer").html(component);
+    $(".privacy").html(privacycom())
   }
   if(data.length > 3){
      scroll++
@@ -94,11 +106,11 @@ sendreqforresdata(6 , t)
 
 $(window).scroll(function() {
   tHeight = document.body.offsetHeight;
-rHeight = Math.floor(window.innerHeight + window.scrollY)
+  rHeight = Math.floor(window.innerHeight + window.scrollY)
 if($("#searchquery").val() != ""){
      searchingquery( $("#searchquery").val() , 3 , t)
     }
-else if (rHeight > tHeight  && !nodata ) {
+else if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)   && !nodata ) {
        sendreqforresdata(3 , t) ;
     }
 });
@@ -133,6 +145,7 @@ $(document).on("click",".col", function() {
   console.log(this.id)
   window.location = "food.html"
 });
+
 
 
 
